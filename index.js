4dfecf51557161056aac2769d2d427b364d667fa
Will Jarvis <will@toohotlimited.com>
2019-03-27 18:46:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+  fetch ('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
