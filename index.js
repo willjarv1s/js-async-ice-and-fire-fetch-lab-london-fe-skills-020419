1a80c446f7a7dc357cf6f354bced0bcc8823ab3d
Will Jarvis <will@toohotlimited.com>
2019-03-27 18:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   fetch ('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(response => response.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
